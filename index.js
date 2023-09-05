13893070fd6dc1f3de3341bccf32fd8d146642ba
User <user@pop-os.localdomain>
2023-09-06 00:10:28 +0700

    ok

--- index.js
+++ index.js
@@ -16,21 +16,12 @@ app.get("/q/:q", (req, res) => {
     res.end(err)
   })
 
-  fs.readFile('./q.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const code = data.split(tag)
-    let txt = code[1]+tag+code[2]+tag+code[3]+tag+code[4]+tag+code[5]+tag+code[6]+tag+code[7]+tag+code[8]+tag+code[9]+tag+code[10]+tag+code[11]+tag+code[12]+tag+code[13]+tag+code[14]+tag+code[15]+tag+code[16]+tag+code[17]+tag+code[18]+tag+code[19]+tag+code[20]+tag
-    txt += q
-    try {
-      fs.writeFileSync('./q.txt', "ok");
-      console.log(1);
-    } catch (err) {
-      return
-    }
-  });
+  try {
+    fs.writeFileSync('./q.txt', "ok");
+    console.log(1);
+  } catch (err) {
+    return
+  }
 
   
   res.send(req.params.q);
