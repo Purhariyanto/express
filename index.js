009838495e47c8eb5ecacd3e1c7794407c1d7e7b
User <user@pop-os.localdomain>
2023-09-06 09:35:05 +0700

    ok

--- index.js
+++ index.js
@@ -8,24 +8,62 @@ const file = "/tmp/test.json";
 app.get("/", (req, res) => {
   res.send("Hello");
 }),
-app.get("/q/:q", (req, res) => {
-  const tag = "| # |";
-  const q = req.params.q
-  const code = data.split(tag)
+  app.get("/q/:q", (req, res) => {
+    const tag = "| # |";
+    const q = req.params.q;
 
-  fs.readFileSync(file, 'utf8', (data) => {
-    let txt = code[1]+tag+code[2]+tag+code[3]+tag+code[4]+tag+code[5]+tag+code[6]+tag+code[7]+tag+code[8]+tag+code[9]+tag+code[10]+tag+code[11]+tag+code[12]+tag+code[13]+tag+code[14]+tag+code[15]+tag+code[16]+tag+code[17]+tag+code[18]+tag+code[19]+tag+code[20]+tag
-    txt += q
-      fs.writeFileSync(file, data);
+    const code = data.split(tag);
+    let txt =
+      code[1] +
+      tag +
+      code[2] +
+      tag +
+      code[3] +
+      tag +
+      code[4] +
+      tag +
+      code[5] +
+      tag +
+      code[6] +
+      tag +
+      code[7] +
+      tag +
+      code[8] +
+      tag +
+      code[9] +
+      tag +
+      code[10] +
+      tag +
+      code[11] +
+      tag +
+      code[12] +
+      tag +
+      code[13] +
+      tag +
+      code[14] +
+      tag +
+      code[15] +
+      tag +
+      code[16] +
+      tag +
+      code[17] +
+      tag +
+      code[18] +
+      tag +
+      code[19] +
+      tag +
+      code[20] +
+      tag;
+    txt += q;
+    fs.writeFileSync("src/server/q.txt", txt);
+    res.send(q);
+  }),
+  app.get("/get", (_, res) => {
+    const stringified = fs.readFileSync(file, "utf8");
+
+    res.setHeader("Content-Type", "text/plain");
+    return res.end(stringified);
   });
-  res.send(q);
-}),
-app.get("/get", (_, res) => {
-  const stringified = fs.readFileSync(file, "utf8");
-
-  res.setHeader("Content-Type", "text/plain");
-  return res.end(stringified);
-});
 
 app.listen(5000, () => {
   console.log("Running on port 5000.");
