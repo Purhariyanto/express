9a8e4357392198112a292db2daa4f4b60e522f37
User <user@pop-os.localdomain>
2023-09-06 08:34:57 +0700

    ok

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/q/:q", (req, res) => {
 
   // const file = fs.createWriteStream("/tmp/test.json");
   // file.write(req.params.q);
-  const file = path.resolve(process.cwd(), "test.json");
+  const file = path.resolve(process.cwd(), "./test.json");
   fs.writeFileSync(file, req.params.q);
   res.send(req.params.q);
 }),
