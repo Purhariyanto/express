7507815c5aacdfaff28357a1d259340e5e8b6ec3
User <user@pop-os.localdomain>
2023-09-06 09:08:03 +0700

    ok

--- index.js
+++ index.js
@@ -3,26 +3,21 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+const file = path.join(process.cwd(), "test.json");
 
 app.get("/", (req, res) => {
   res.send("Hello");
 }),
 app.get("/q/:q", (req, res) => {
-  
-
-  // const file = fs.createWriteStream("/tmp/test.json");
-  // file.write(req.params.q);
-  const file = path.resolve(process.cwd(), "test.json");
   fs.writeFileSync(file, req.params.q);
   res.send(req.params.q);
 }),
-  app.get("/get", (_, res) => {
-    const file = path.resolve(process.cwd(), "test.json");
-    const stringified = fs.readFileSync(file, "utf8");
+app.get("/get", (_, res) => {
+  const stringified = fs.readFileSync(file, "utf8");
 
-    res.setHeader("Content-Type", "text/plain");
-    return res.end(stringified);
-  });
+  res.setHeader("Content-Type", "text/plain");
+  return res.end(stringified);
+});
 
 app.listen(5000, () => {
   console.log("Running on port 5000.");
