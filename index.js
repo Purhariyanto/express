25c575dc406341184556f9a05e1433366e9e5c05
User <user@pop-os.localdomain>
2023-09-06 09:44:57 +0700

    ok

--- index.js
+++ index.js
@@ -11,20 +11,22 @@ app.get("/", (req, res) => {
   app.get("/q/:q", (req, res) => {
     const tag = "| # |";
     const q = req.params.q;
-    fs.readFileSync(file, 'utf8', (err, data) => {
-      if (err) {
+    
+      if (fs.existsSync(file)) {
         const data = "ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok1| # |ok2"
         const code = data.split(tag);
         let txt = code[1]+tag+code[2]+tag+code[3]+tag+code[4]+tag+code[5]+tag+code[6]+tag+code[7]+tag+code[8]+tag+code[9]+tag+code[10]+tag+code[11]+tag+code[12]+tag+code[13]+tag+code[14]+tag+code[15]+tag+code[16]+tag+code[17]+tag+code[18]+tag+code[19]+tag+code[20]+tag;
         txt += q;
         fs.writeFileSync(file, txt);
       } else {
-        const code = data.split(tag);
-        let txt = code[1]+tag+code[2]+tag+code[3]+tag+code[4]+tag+code[5]+tag+code[6]+tag+code[7]+tag+code[8]+tag+code[9]+tag+code[10]+tag+code[11]+tag+code[12]+tag+code[13]+tag+code[14]+tag+code[15]+tag+code[16]+tag+code[17]+tag+code[18]+tag+code[19]+tag+code[20]+tag;
-        txt += q;
-        fs.writeFileSync(file, txt);
+        fs.readFileSync(file, 'utf8', (err, data) => {
+          const code = data.split(tag);
+          let txt = code[1]+tag+code[2]+tag+code[3]+tag+code[4]+tag+code[5]+tag+code[6]+tag+code[7]+tag+code[8]+tag+code[9]+tag+code[10]+tag+code[11]+tag+code[12]+tag+code[13]+tag+code[14]+tag+code[15]+tag+code[16]+tag+code[17]+tag+code[18]+tag+code[19]+tag+code[20]+tag;
+          txt += q;
+          fs.writeFileSync(file, txt);
+        });
       }
-    });
+    
     
     res.send(q);
   }),
