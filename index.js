5a7290b5c69cae0ac74727284a5f4797a7ac6b47
User <user@pop-os.localdomain>
2023-09-06 09:59:58 +0700

    ok

--- index.js
+++ index.js
@@ -18,13 +18,12 @@ app.get("/", (req, res) => {
         fs.writeFileSync(file, data);
         ok = "1"
       } else {
-        fs.readFileSync(file, 'utf8', (data) => {
+          const data = fs.readFileSync(file, "utf8");
           const code = data.split(tag);
           ok = "2"
           let txt = code[1]+tag+code[2]+tag+code[3]+tag+code[4]+tag+code[5]+tag+code[6]+tag+code[7]+tag+code[8]+tag+code[9]+tag+code[10]+tag+code[11]+tag+code[12]+tag+code[13]+tag+code[14]+tag+code[15]+tag+code[16]+tag+code[17]+tag+code[18]+tag+code[19]+tag+code[20]+tag;
           txt += q;
           fs.writeFileSync(file, txt);
-        });
       }
     
     
