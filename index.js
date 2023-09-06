0d13a245cdb947d6b8a3750b8cb4fb1e1e4ae559
User <user@pop-os.localdomain>
2023-09-06 09:20:41 +0700

    ok

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const file = path.join(process.cwd(), "/test.json");
+const file = path.join(process.cwd(), "/tmp/test.json");
 
 app.get("/", (req, res) => {
   res.send("Hello");
 }),
 app.get("/q/:q", (req, res) => {
-  const stringified = fs.writeFileSync(file, req.params.q);
-  res.setHeader("Content-Type", "text/plain");
-  return res.end(stringified);
+  fs.writeFileSync(file, req.params.q);
+  res.send(req.params.q);
 }),
 app.get("/get", (_, res) => {
   const stringified = fs.readFileSync(file, "utf8");
