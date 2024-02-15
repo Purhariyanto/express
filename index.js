4acc0a87d232bab00e78871de028a9dc36b448bf
User <user@pop-os.localdomain>
2024-02-15 22:44:12 +0700

    first commit

--- index.js
+++ index.js
@@ -12,49 +12,42 @@ const client = new TwitterApi({
 
 const twitterClient = client.readWrite;
 
-const tweet = async (isi) => {
-  try {
-    await twitterClient.v2.tweet(isi);
-    console.log("sukses");
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-// URL situs web yang akan diakses
-const url = "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp.json";
-
-// Fungsi untuk melakukan permintaan GET menggunakan Axios
-async function getData() {
-  try {
-    // Mengirim permintaan GET ke situs web
-    const response = await axios.get(url);
-
-    // Menampilkan data hasil respons
-    const c1 = response.data.chart["100025"];
-    const c2 = response.data.chart["100026"];
-    const c3 = response.data.chart["100027"];
-    const persen = response.data.chart["persen"];
-    const total = c1 + c2 + c3;
-    const persentaseData1 = ((c1 / total) * 100).toFixed(2);
-    const persentaseData2 = ((c2 / total) * 100).toFixed(2);
-    const persentaseData3 = ((c3 / total) * 100).toFixed(2);
-    const progres = response.data.progres["progres"];
-    const totaltps = response.data.progres["total"];
-    const persentps = ((progres / totaltps) * 100).toFixed(2);
-    const waktu = response.data.ts;
-    const text = `HASIL HITUNG SUARA PEMILU PRESIDEN & WAKIL PRESIDEN RI 2024 #Pemilu2024\n\nVersi: ${waktu} Progress: ${progres} dari ${totaltps} TPS (${persentps}%)\n\n01 : Suara ${c1} (${persentaseData1}%)\n\n02 : Suara ${c2} (${persentaseData2}%)\n\n03 : Suara ${c3} (${persentaseData3}%)\n\nSumber: kpu.go.id
-    `;
-    tweet(text);
-  } catch (error) {
-    console.error("Terjadi kesalahan:", error.message);
-  }
-}
+const url = "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp.json"
 
 const app = express();
 app.get("/", (req, res) => {
+  async function getData() {
+    try {
+      const response = await axios.get(url);
+      const c1 = response.data.chart["100025"];
+      const c2 = response.data.chart["100026"];
+      const c3 = response.data.chart["100027"];
+      const total = c1 + c2 + c3;
+      const persentaseData1 = ((c1 / total) * 100).toFixed(2);
+      const persentaseData2 = ((c2 / total) * 100).toFixed(2);
+      const persentaseData3 = ((c3 / total) * 100).toFixed(2);
+      const progres = response.data.progres["progres"];
+      const totaltps = response.data.progres["total"];
+      const persentps = ((progres / totaltps) * 100).toFixed(2);
+      const waktu = response.data.ts;
+      const text = `HASIL HITUNG SUARA PEMILU PRESIDEN & WAKIL PRESIDEN RI 2024 #Pemilu2024\n\nVersi: ${waktu} Progress: ${progres} dari ${totaltps} TPS (${persentps}%)\n\n01 : Suara ${c1} (${persentaseData1}%)\n\n02 : Suara ${c2} (${persentaseData2}%)\n\n03 : Suara ${c3} (${persentaseData3}%)\n\nSumber: kpu.go.id
+      `;
+
+      const tweet = async (isi) => {
+        try {
+          await twitterClient.v2.tweet(isi);
+          res.send(isi)
+        } catch (e) {
+          res.status(e.code).send("Terjadi kesalahan");
+        }
+      };
+
+      tweet(text)
+    } catch (error) {
+      res.send("Terjadi kesalahan");
+    }
+  }
   getData();
-  res.send("Hello");
 }),
   app.get("/google4b0784f451721b6b.html", (req, res) => {
     res.sendFile(path.join(__dirname, "google4b0784f451721b6b.html"));
@@ -64,5 +57,4 @@ app.listen(8000, () => {
   console.log("Running on port 8000.");
 });
 
-// Export the Express API
 module.exports = app;
